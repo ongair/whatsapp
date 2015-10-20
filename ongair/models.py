d7436e4a0a104c268f23b2bc02612a34a7548a48
Trevor Kimenye <kimenye@gmail.com>
2015-10-20 15:05:12 +0300

    Added job log model

--- ongair/models.py
+++ ongair/models.py
@@ -10,3 +10,31 @@ class Account(Base):
   phone_number = Column(String(255))
   setup = Column(Boolean())
   name = Column(String(255))
+
+class Job(Base):
+  __tablename__ = 'job_logs'
+  id = Column(Integer, primary_key=True)
+
+  method = Column(String(255))
+  targets = Column(String())
+  args = Column(String())
+  sent = Column(Boolean())
+  scheduled_time = Column(DateTime())
+  whatsapp_message_id = Column(String(255))
+  received = Column(String(255))
+  receipt_timestamp = Column(DateTime())
+  message_id = Column(Integer)
+  broadcast_part_id = Column(Integer)
+  account_id = Column(Integer)
+  runs = Column(Integer)
+  next_job_id = Column(Integer)
+  asset_id = Column(Integer)
+  off_line = Column(Boolean())
+  pending = Column(Boolean())
+
+  def __init__(self, method, targets, sent, args, scheduled_time):
+    self.method = method
+    self.targets = targets
+    self.sent = sent
+    self.args = args
+    self.scheduled_time = scheduled_time
