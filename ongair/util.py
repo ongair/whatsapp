c2395705d003b9652843956f51a34222bca0c9f1
Trevor <kimenye@gmail.com>
2015-10-21 08:45:23 +0000

    Capturing warnings in log file

--- ongair/util.py
+++ ongair/util.py
@@ -4,6 +4,7 @@ def get_env(key):
   return os.environ.get(key).encode('utf-8')
 
 def setup_logging(phone_number):
+  logging.captureWarnings(True)
   env = get_env('env')  
   logging.basicConfig(level=logging.INFO,
     format='%(asctime)s %(name)-12s %(levelname)-8s %(message)s',
