d69401e5de7f7e940d12aa47826189d45170f760
Trevor Kimenye <kimenye@gmail.com>
2015-10-21 11:43:35 +0300

    Set pwd

--- ongair/util.py
+++ ongair/util.py
@@ -8,7 +8,7 @@ def setup_logging(phone_number):
   logging.basicConfig(level=logging.INFO,
     format='%(asctime)s %(name)-12s %(levelname)-8s %(message)s',
     datefmt='%m-%d %H:%M',
-    filename="logs/%s.%s.log" %(phone_number, env),
+    filename="%s/logs/%s.%s.log" %(get_env('pwd'), phone_number, env),
     filemode='w')
 
 def post_to_server(url, phone_number, payload):
