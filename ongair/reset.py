a06513fd9ec735a6fde27491f8a3c98e17926d53
Trevor Kimenye <kimenye@gmail.com>
2015-10-21 12:38:16 +0300

    Added reset script

---
+++ ongair/reset.py
@@ -0,0 +1,58 @@
+from yowsup.layers.auth                        import YowAuthenticationProtocolLayer
+from yowsup.layers.protocol_messages           import YowMessagesProtocolLayer
+from yowsup.layers.protocol_receipts           import YowReceiptProtocolLayer
+from yowsup.layers.protocol_acks               import YowAckProtocolLayer
+from yowsup.layers.network                     import YowNetworkLayer
+from yowsup.layers.coder                       import YowCoderLayer
+from yowsup.layers.interface                           import YowInterfaceLayer, ProtocolEntityCallback
+from yowsup.layers.protocol_messages.protocolentities  import TextMessageProtocolEntity
+from yowsup.layers.protocol_receipts.protocolentities  import OutgoingReceiptProtocolEntity
+from yowsup.layers.protocol_acks.protocolentities      import OutgoingAckProtocolEntity
+from yowsup.stacks import YowStack
+from yowsup.common import YowConstants
+from yowsup.layers import YowLayerEvent
+from yowsup.stacks import YowStack, YOWSUP_CORE_LAYERS
+from yowsup import env
+import logging
+
+
+class EchoLayer(YowInterfaceLayer):
+
+    @ProtocolEntityCallback("message")
+    def onMessage(self, messageProtocolEntity):
+        #send receipt otherwise we keep receiving the same message over and over
+
+        if True:
+            receipt = OutgoingReceiptProtocolEntity(messageProtocolEntity.getId(), messageProtocolEntity.getFrom(), 'read', messageProtocolEntity.getParticipant())
+
+            outgoingMessageProtocolEntity = TextMessageProtocolEntity(
+                messageProtocolEntity.getBody(),
+                to = messageProtocolEntity.getFrom())
+
+            self.toLower(receipt)
+            # self.toLower(outgoingMessageProtocolEntity)
+
+    @ProtocolEntityCallback("receipt")
+    def onReceipt(self, entity):
+        ack = OutgoingAckProtocolEntity(entity.getId(), "receipt", entity.getType(), entity.getFrom())
+        self.toLower(ack)
+
+
+CREDENTIALS = ("phone_number", "passwor") # replace with your phone and password
+logging.basicConfig(level=logging.DEBUG)
+
+if __name__==  "__main__":
+    layers = (
+        EchoLayer,
+        (YowAuthenticationProtocolLayer, YowMessagesProtocolLayer, YowReceiptProtocolLayer, YowAckProtocolLayer)
+    ) + YOWSUP_CORE_LAYERS
+
+    stack = YowStack(layers)
+    stack.setProp(YowAuthenticationProtocolLayer.PROP_CREDENTIALS, CREDENTIALS)         #setting credentials
+    stack.setProp(YowNetworkLayer.PROP_ENDPOINT, YowConstants.ENDPOINTS[0])    #whatsapp server address
+    stack.setProp(YowCoderLayer.PROP_DOMAIN, YowConstants.DOMAIN)              
+    stack.setProp(YowCoderLayer.PROP_RESOURCE, env.CURRENT_ENV.getResource())          #info about us as WhatsApp client
+
+    stack.broadcastEvent(YowLayerEvent(YowNetworkLayer.EVENT_STATE_CONNECT))   #sending the connect signal
+
+    stack.loop() #this is the program mainloop
