f4136b01536526851ea9cc76e64bb39932866af2
Trevor Kimenye <kimenye@gmail.com>
2015-10-22 15:52:25 +0300

    Fixed checker

--- ongair/checker.py
+++ ongair/checker.py
@@ -1,3 +1,4 @@
+
 from dotenv import load_dotenv
 from util import get_env
 from models import Account, Job, Message
@@ -6,13 +7,12 @@ from sqlalchemy import create_engine
 
 import sys, getopt, os, argparse, commands
 
-
 def main(argv):
   parser = argparse.ArgumentParser(description='Description of your program')
   parser.add_argument('-c','--config', help='Path of the config file', required=True)
-  
+
   args = vars(parser.parse_args())
-  load_dotenv(args['config'])  
+  load_dotenv(args['config'])
 
   url = get_env('db')
   db = create_engine(url, echo=False, pool_size=1, pool_timeout=600,pool_recycle=600)
@@ -20,8 +20,14 @@ def main(argv):
   sess = session()
 
   accounts = sess.query(Account).filter_by(setup= True).all()
+  print("Accounts : %s" % len(accounts))
   for acc in accounts:
     output = commands.getoutput("service ongair-%s status" %acc.phone_number)
 
     if "stop/waiting" in output:
       print "Account %s-%s needs to start" %(acc.phone_number, acc.name)
+
+
+if __name__ == "__main__":
+  main(sys.argv[1:])
+
