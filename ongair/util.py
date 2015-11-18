99c63bad54c26fa11c39147b1fc3686332d367a3
Trevor Kimenye <kimenye@gmail.com>
2015-11-18 08:39:58 +0300

    Filemode append

--- ongair/util.py
+++ ongair/util.py
@@ -25,7 +25,7 @@ def setup_logging(phone_number):
     format='%(asctime)s %(name)-12s %(levelname)-8s %(message)s',
     datefmt='%m-%d %H:%M',
     filename="%s/logs/%s.%s.log" %(get_env('pwd'), phone_number, env),
-    filemode='w')
+    filemode='a')
 
 def post_to_server(url, phone_number, payload):
   try:
