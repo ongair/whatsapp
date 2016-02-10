ac10912e54f7a47fce88e56ca6fed81092524fb0
Kenneth Kinyanjui <kenmbugua@gmail.com>
2016-02-10 22:05:52 +0300

    Adding setup.py with dependencies

---
+++ setup.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+from __future__ import print_function
+from setuptools import setup, find_packages
+import ongair
+
+dependencies = ['click', 'docopt', 'mandrill', 'MySQL-python', 'ordereddict', 'Pillow',
+                'protobuf', 'pycrypto', 'pubnub', 'python-axolotl', 'python-axolotl-curve25519',
+                'python-dateutil','python-dotenv', 'requests','six','SQLAlchemy', 'wheel','yowsup2']
