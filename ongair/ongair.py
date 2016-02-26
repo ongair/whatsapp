9e65153385914ad677a70d158d84caf05a443780
Trevor Kimenye <kimenye@gmail.com>
2016-02-26 14:49:08 +0300

    Notifications for location

--- ongair/ongair.py
+++ ongair/ongair.py
@@ -128,6 +128,13 @@ class OngairLayer(YowInterfaceLayer):
         data = { 'location' : { 'latitude' : entity.getLatitude(), 'longitude' : entity.getLongitude(), 'external_contact_id' : by, 'external_message_id' : id, 'name' : name, 'source': 'WhatsApp' }}
         self._post('locations', data)
 
+        self._sendRealtime({
+            'type': 'location',
+            'external_contact_id': by,
+            'latitude': entity.getLatitude(),
+            'longitude': entity.getLongitude() 
+        })
+
     # This is called by onMessage when a media type is received
     def onMediaMessage(self, entity):
         by = entity.getFrom(False)
