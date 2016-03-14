e78d5eec540116c309edd33008c13199c740cd2e
Trevor Kimenye <kimenye@gmail.com>
2016-03-14 15:57:43 +0300

    Removed option for running unencrypted accounts

--- ongair/client.py
+++ ongair/client.py
@@ -21,44 +21,29 @@ from stack import OngairStackBuilder
 
 
 class Client:
-    def __init__(self, phone_number, encrypted=True):
+    def __init__(self, phone_number):
         self.connected = False
-        self.encrypted = encrypted
         self.phone_number = phone_number
 
         setup_logging(phone_number)
 
-    def loop(self):
-        if self.encrypted:
-            stackBuilder = YowStackBuilder()
-            # Create the default stack (a pile of layers) and add the Ongair Layer to the top of the stack
-            stack = stackBuilder.pushDefaultLayers(True).push(OngairLayer).build()
+    def loop(self):        
+        stackBuilder = YowStackBuilder()
+        # Create the default stack (a pile of layers) and add the Ongair Layer to the top of the stack
+        stack = stackBuilder.pushDefaultLayers(True).push(OngairLayer).build()
 
-            ping_interval = get_env('ping_interval')
+        ping_interval = int(get_env('ping_interval'))
 
-            # Set the phone number as a property that can be read by other layers
-            stack.setProp(YowIqProtocolLayer.PROP_PING_INTERVAL, ping_interval)
-            stack.setProp('ongair.account', self.phone_number)
-            stack.setProp(YowNetworkLayer.PROP_ENDPOINT, YowConstants.ENDPOINTS[0])  # whatsapp server address
-            stack.setProp(YowCoderLayer.PROP_DOMAIN, YowConstants.DOMAIN)
-            stack.setProp(YowCoderLayer.PROP_RESOURCE,
-                          env.CURRENT_ENV.getResource())  # info about us as WhatsApp client
+        # Set the phone number as a property that can be read by other layers
+        stack.setProp(YowIqProtocolLayer.PROP_PING_INTERVAL, ping_interval)
+        stack.setProp('ongair.account', self.phone_number)
+        stack.setProp(YowNetworkLayer.PROP_ENDPOINT, YowConstants.ENDPOINTS[0])  # whatsapp server address
+        stack.setProp(YowCoderLayer.PROP_DOMAIN, YowConstants.DOMAIN)
+        stack.setProp(YowCoderLayer.PROP_RESOURCE,
+                      env.CURRENT_ENV.getResource())  # info about us as WhatsApp client
 
-            # Broadcast the login event. This gets handled by the OngairLayer
-            stack.broadcastEvent(YowLayerEvent(OngairLayer.EVENT_LOGIN))
+        # Broadcast the login event. This gets handled by the OngairLayer
+        stack.broadcastEvent(YowLayerEvent(OngairLayer.EVENT_LOGIN))
 
-            # Run the asyncore loop
-            stack.loop(timeout=5, discrete=0.5)  # this is the program mainloop
-        else:
-            # in the case that it is not encrypted
-            # TODO: Refactor this if necessary
-            stackBuilder = OngairStackBuilder()
-            stack = stackBuilder.pushDefaultLayers().push(OngairLayer).build()
-
-            stack.setProp('ongair.account', self.phone_number)
-            stack.setProp(YowNetworkLayer.PROP_ENDPOINT, YowConstants.ENDPOINTS[0])  # whatsapp server address
-            stack.setProp(YowCoderLayer.PROP_DOMAIN, YowConstants.DOMAIN)
-            stack.setProp(YowCoderLayer.PROP_RESOURCE,
-                          env.CURRENT_ENV.getResource())  # info about us as WhatsApp client
-            stack.broadcastEvent(YowLayerEvent(OngairLayer.EVENT_LOGIN))
-            stack.loop(timeout=5, discrete=0.5)  # this is the program mainloop
+        # Run the asyncore loop
+        stack.loop(timeout=5, discrete=0.5)  # this is the program mainloop        
